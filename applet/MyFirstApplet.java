1f3e37fdaf6c90951563ed1b1e2a8ed067f6ffa7
Ishika <ishikakv52@gmail.com>
2026-09-22 14:55:58 +0530

    Applet first

---
+++ applet/MyFirstApplet.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+
+public class MyFirstApplet extends Panel {
+    public MyFirstApplet() {
+        add(new Label("Hello"));
+    }
+}
---
+++ applet/MyFirstLauncher.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class MyFirstLauncher {
+    public static void main(String[] args) {
+        Frame f = new Frame("My Window");
+        
+        MyFirstApplet applet = new MyFirstApplet();
+        f.add(applet);
+        
+        f.setSize(300, 200);
+        f.setVisible(true);
+    }
+}
